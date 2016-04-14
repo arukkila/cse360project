801a059d2f541828566f6d1052c24d32a18cc974
Matt Dunning <matthewdunning33@gmail.com>
2016-04-13 17:23:52 -0700

    added player handling

--- controller.java
+++ controller.java
@@ -1,56 +1,129 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class controller{
+/**
+ * controller class controls the game functionality
+ * @author Matt Dunning
+ *
+ */
+public class controller extends Player{
+	
+	controller(String name)
+	{
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	// Class Variables or Fields
+	// You declare constants with final
 	
 	public final String LOSERS = "ALL OF YOU LOST";
 	public final String WINNER = "CONGRATS YOU ARE THE WINNER";
 	
 	// private fields are not visible outside of the class
-	private String masterList[];
-	private String currentList[];
+	private Player[] masterList;
+	private Player[] currentList;
 	private int score = 0;
-	private boolean currentPlayer = true;
-	
+	private int currentPlayer = 0;
 	// public variables are visible outside of the class
 	public String name;
 	
-	public String[] getMasterList()
+	/**
+	 * contains the master list
+	 * @return the current player list without any altercations
+	 */
+	public Player[] getMasterList()
 	{
 		return masterList;
 	}
+
 	
-	public String[] getCurrentList()
+	public int getCurrentPlayerCount(Player[] currentList){
+		
+		int numberOfPlayers = 0;
+		
+		for(int i = 0; i < currentList.length; i++ )
+		{
+			numberOfPlayers++;
+		}
+		return numberOfPlayers;
+	}
+	/**
+	 * function that has current list
+	 * @return returns the current list with player removals if any
+	 */
+	public Player[] getCurrentList()
 	{
 		return currentList;
 	}
 	
-	public boolean checkCurrentPlayer()
+	/**
+	 * function to iterate through the players
+	 * @return the name of the current player
+	 */
+	public Player currentPlayer()
 	{
+		// listen for roll button
+		Player currentRoller = null;
 		
+		if(currentPlayer == 0)
+		{
+			currentRoller = currentList[0];
+			currentPlayer++;
+		}
+		else if(currentPlayer == 1)
+		{
+			currentRoller = currentList[1];
+			currentPlayer++;
+		}
+		else if(currentPlayer == 2)
+		{
+			currentRoller = currentList[2];
+			currentPlayer++;
+		}
+		else if(currentPlayer == 3)
+		{
+			currentRoller = currentList[3];
+			currentPlayer++;
+		}
+		
+		return currentRoller;
+	
 	}
 	
 	
-	
-	public String getCurrentPlayer()
+	/**
+	 * gets the current player
+	 * @return returns string name of current player
+	 */
+	public Player getCurrentPlayer(Player[] currentList)
 	{
+		Player playerRolling = currentPlayer();
+		
 		for(int i = 0; i < currentList.length; i++)
 		{
-			if(checkCurrentPlayer() == true)
+			if(playerRolling == currentList[i])
 			{
-				return currentList[i];
+				 playerRolling = currentList[i];
 			}
 		}
-		return "player not found";
 		
+		return playerRolling;
 	}
 	
-	public String[] getOtherPlayers()
+	/**
+	 * function helper for reseting other players scores
+	 * @return all players except for the current player
+	 */
+	public Player[] getOtherPlayers()
 	{
-		String tempList[] = new String[3];
+		Player playerRolling = currentPlayer();
+		Player tempList[] = new Player[3];
 		int playerPos = 0;
 		for(int listIndex = 0; listIndex < currentList.length; listIndex++)
 		{
-			while(checkCurrentPlayer() == false)
+			while(playerRolling != currentList[listIndex])
 			{
 				tempList[playerPos] = currentList[listIndex];
 				playerPos++;
@@ -60,25 +133,42 @@ public class controller{
 		
 	}
 	
+	/**
+	 * scores of players
+	 * @return returns the scores of individual player
+	 */
 	public int getScore()
 	{
 		return score;
 	}
 	
 	
-	public void setMasterList(String[] players)
+	/**
+	 * assigns the master list that is not changed
+	 * @param players list of the initial players
+	 */
+	public void setMasterList(Player[] players)
 	{
-		 masterList = new String[4];
+		 masterList = new Player[4];
 		 System.arraycopy(players, 0, masterList, 0, players.length);
 	}
 	
+	/**
+	 * sets up the current list that can be altered 
+	 * if a player needs to be removed
+	 * @param players list of players from start
+	 */
 	public void setCurrentList(String[] players)
 	{
-		currentList = new String[4];
+		 currentList = new Player[4];
 		 System.arraycopy(players, 0, masterList, 0, players.length);
 	}
 	
-	public void removePlayer(String player)
+	/**
+	 * removes a player from the game
+	 * @param player the string name of the player to be removed
+	 */
+	public void removePlayer(Player player)
 	{
 		
 		if(Arrays.asList(currentList).contains(player))
@@ -88,11 +178,19 @@ public class controller{
 		
 	}
 	
+	/**
+	 * terminates the program
+	 */
 	public void exitProgram()
 	{
 		System.exit(0);
 	}
 	
+	/**
+	 * after the game is finished the choice is made to
+	 * start a new game, terminate the program, or view the stats
+	 * @param choice the option that is decided
+	 */
 	public void gameOver(int choice)
 	{
 		// if player wants to play again 
@@ -123,7 +221,11 @@ public class controller{
 		
 	}
 	
-	
+	/**
+	 * checks against the games rules and makes decisions based on 
+	 * the rule 
+	 * @param ruleNumber the rule that has happened from the roll
+	 */
 	public void ruleCheck(int ruleNumber)
 	{
 	
@@ -138,22 +240,15 @@ public class controller{
 		 {
 					 
 			 //check who the current player is
-			 String currentPlayer = getCurrentPlayer();
+			
 			 
 			 // remove player from the game
-			  removePlayer(currentPlayer);
+			
 		 }
 		 // player roled 3 1s reset everyone else
 		 else if(ruleNumber == 1)
 		 {
-			 //get current player
-			 String[] otherPlayers = getOtherPlayers();
 			 
-			 // reset the other 3 players
-			 for(int resetIndex = 0 ; resetIndex < otherPlayers.length; resetIndex++)
-			 {
-				 
-			 }
 			 
 			 
 		 }
@@ -161,11 +256,9 @@ public class controller{
 		
 	}
 	
-	
-	public controller()
-	{
+	/**
+	 * default construct
+	 */
 		
-	}
 	
-	
-	
+}
