e0e5ef0244a9596d9e5a9d4f9ede172b9bce3d46
Denise Perry <dbperry2@asu.edu>
2016-04-15 22:18:37 -0700

    Minor formatting issues

--- src/cse360pro1/RulesGuiInterface.java
+++ src/cse360pro1/RulesGuiInterface.java
@@ -5,15 +5,18 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
+ * Graphic User Interface for viewing the rules of the game
  * 
  * @author Denise Perry
  */
-
 public class RulesGuiInterface extends JPanel
 {
 	private final int WIDTH = 500, HEIGHT = 500;
 	private JButton okay;
 
+	/**
+	 * Creates a new RulesGuiInterface
+	 */
 	public RulesGuiInterface()
 	{
 		okay = new JButton("Got it");
@@ -21,8 +24,6 @@ public class RulesGuiInterface extends JPanel
 
 		add(okay);
 
-		Color color = Color.decode("0xEEEEEE");
-		setBackground(color);
 		setPreferredSize(new Dimension(500, 500));
 		
 		this.setLayout(new BorderLayout());
@@ -47,33 +48,19 @@ public class RulesGuiInterface extends JPanel
 		page.setColor(Color.black);
 		page.setFont(new Font("TimesRoman", Font.PLAIN, 20));
 		
-		String ruleList1 = "1. Players roll 3 dice at a time.";
-		String ruleList2 = "2. Players must roll and record those stats every turn.";
-		String ruleList3 = "3. Any player to roll 3 1s automatically loses and";
-		String ruleList3a = "must wait for the next game.";
-		String ruleList4 = "4. The first player to roll all 6s wins the game";
-		String ruleList4a = "no matter the scores.";
-		String ruleList5 = "5. If a player rolls all 3s all other players scores";
-		String ruleList5a = "reset to 0.";
-		String ruleList6 = "6. If a player rolls two of a kind they can roll again.";
-		String ruleList7 = "7. The first player with a total ≥ 100 wins the game.";
-		String ruleList8 = "8. The end of the game will rank all of the players on";
-		String ruleList8a = "who is closest to 100.";
-		String ruleList9 = "9. Game requires four players to start.";
+		page.drawString("1. Players roll 3 dice at a time.", WIDTH/2 - 200, HEIGHT/2 - 200);
+		page.drawString("2. Players must roll and record those stats every turn.", WIDTH/2 - 200, HEIGHT/2 - 200 + 30);
+		page.drawString("3. Any player to roll 3 1s automatically loses and", WIDTH/2 - 200, HEIGHT/2 - 200 + 60);
+		page.drawString("must wait for the next game.", WIDTH/2 - 180, HEIGHT/2 - 200 + 90);
+		page.drawString("4. The first player to roll all 6s wins the game", WIDTH/2 - 200, HEIGHT/2 - 200 + 120);
+		page.drawString("no matter the scores.", WIDTH/2 - 180, HEIGHT/2 - 200 + 150);
+		page.drawString("5. If a player rolls all 3s all other players scores", WIDTH/2 - 200, HEIGHT/2 - 200 + 180);
+		page.drawString("reset to 0.", WIDTH/2 - 180, HEIGHT/2 - 200 + 210);
+		page.drawString("6. If a player rolls two of a kind they can roll again.", WIDTH/2 - 200, HEIGHT/2 - 200 + 240);
+		page.drawString("7. The first player with a total >= 100 wins the game.", WIDTH/2 - 200, HEIGHT/2 - 200 + 270);
+		page.drawString("8. The end of the game will rank all of the players on", WIDTH/2 - 200, HEIGHT/2 - 200 + 300);
+		page.drawString("who is closest to 100.", WIDTH/2 - 180, HEIGHT/2 - 200 + 330);
+		page.drawString("9. Game requires four players to start.", WIDTH/2 - 200, HEIGHT/2 - 200 + 360);
 
-		page.drawString(ruleList1, WIDTH/2 - 200, HEIGHT/2 - 200);
-		page.drawString(ruleList2, WIDTH/2 - 200, HEIGHT/2 - 200 + 30);
-		page.drawString(ruleList3, WIDTH/2 - 200, HEIGHT/2 - 200 + 60);
-		page.drawString(ruleList3a, WIDTH/2 - 180, HEIGHT/2 - 200 + 90);
-		page.drawString(ruleList4, WIDTH/2 - 200, HEIGHT/2 - 200 + 120);
-		page.drawString(ruleList4a, WIDTH/2 - 180, HEIGHT/2 - 200 + 150);
-		page.drawString(ruleList5, WIDTH/2 - 200, HEIGHT/2 - 200 + 180);
-		page.drawString(ruleList5a, WIDTH/2 - 180, HEIGHT/2 - 200 + 210);
-		page.drawString(ruleList6, WIDTH/2 - 200, HEIGHT/2 - 200 + 240);
-		page.drawString(ruleList7, WIDTH/2 - 200, HEIGHT/2 - 200 + 270);
-		page.drawString(ruleList8, WIDTH/2 - 200, HEIGHT/2 - 200 + 300);
-		page.drawString(ruleList8a, WIDTH/2 - 180, HEIGHT/2 - 200 + 330);
-		page.drawString(ruleList9, WIDTH/2 - 200, HEIGHT/2 - 200 + 360);
-
-		}
+	}
 }
