dbd03046bbf65da409be435e7a5caa30995a6a0a
Tom Gottschlich <TomGottschlich@gmail.com>
2016-04-22 16:31:18 -0700

    Database initial tests

--- src/cse360pro1/DatabaseTester.java
+++ src/cse360pro1/DatabaseTester.java
@@ -7,7 +7,47 @@ import org.junit.Test;
 public class DatabaseTester {
 
 	@Test
-	public void test() {
+	public void testDatabase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerializeObjece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserializeObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSavePlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerForName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllPlayerNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerDatabase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
 		fail("Not yet implemented");
 	}
 
