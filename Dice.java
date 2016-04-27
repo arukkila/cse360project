f8d4b93fad2a7c6a9f9b4fed2fcb95abe0d55a72
Matt Dunning <matthewdunning33@gmail.com>
2016-04-26 18:54:17 -0700

    code and style update

---
+++ Dice.java
@@ -0,0 +1,42 @@
+/**
+ * Class that creates a virtual dice that can roll/generate a random number. 
+ * The number of sides can be changed as desired.
+ * 
+ * @author Thomas Gottschlich for CSE360
+ * @version Apr 8 2016 
+ * @pin 326
+ */
+package cse360pro1;
+
+import java.util.Random;
+
+public class Dice 
+{
+	/**
+	*number stores dice rolled value 
+	*sides keeps the allotted dice faces
+	*/
+	private Random number;
+	private int sides;
+	
+	/**
+	 * Creates a dice with a random number generator.
+	 */
+	Dice(int sides)
+	{
+		this.sides = sides;
+		number = new Random(); 
+	}
+	
+	/**
+	 * Generates a number between 1 and the number of sides given.
+	 * 
+	 * @param sides - number of sides on the dice
+	 * @return number - integer that the dice rolls
+	 */
+	public int roll()
+	{
+		return number.nextInt(this.sides) + 1; 
+	}	
+
+}
