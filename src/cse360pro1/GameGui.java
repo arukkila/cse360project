1bba01cd9b6ef7120a44368d22858b7becc59f27
m-35 <jpsxdec@gmail.com>
2016-04-29 18:53:48 -0700

    Fix: Enter key anywhere in GameGui rolls the dice

--- src/cse360pro1/GameGui.java
+++ src/cse360pro1/GameGui.java
@@ -318,6 +318,7 @@ public class GameGui extends javax.swing.JFrame implements GameGuiInterface {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Dice Simulator 2016");
+        setPreferredSize(new java.awt.Dimension(900, 450));
         addWindowListener(new java.awt.event.WindowAdapter()
         {
             public void windowClosed(java.awt.event.WindowEvent evt)
@@ -350,6 +351,7 @@ public class GameGui extends javax.swing.JFrame implements GameGuiInterface {
 
         getContentPane().add(tableScrollPane, java.awt.BorderLayout.CENTER);
 
+        scorePanel.setPreferredSize(new java.awt.Dimension(130, 100));
         scorePanel.setLayout(new java.awt.GridLayout(2, 0));
 
         currentTotalLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
